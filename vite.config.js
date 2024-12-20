5c3315493b450c8ae619aac0c1ad8868083cb362
hasin01 <81334215+hasin01@users.noreply.github.com>
2024-12-20 13:50:23 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [],
+  base: '/psychologists.services/',
   server: { host: true, port: 8080, },
 })
