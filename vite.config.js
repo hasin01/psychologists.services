80657c094327c97db99c062d46884df0658c1fec
hasin01 <81334215+hasin01@users.noreply.github.com>
2024-12-22 21:40:02 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,46 @@
 import { defineConfig } from 'vite'
+import { glob } from "glob";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-   plugins: [],
-  base: '/psychologists.services/',
-  server: { host: true, port: 8080, },
-})
+export default defineConfig(({ command }) => {
+  return {
+    server: {
+      host: true,
+      port: 8080,
+    },
+
+    base: "/psychologists.services/",
+    define: {
+      [command === "serve" ? "global" : "_global"]: {},
+    },
+    root: "src",
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync("./src/*.html"),
+  
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === "commonHelpers") {
+              return "commonHelpers.js";
+            }
+            return "[name].js";
+          },
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+              return "[name].[ext]";
+            }
+            return "assets/[name]-[hash][extname]";
+          },
+        },
+      },
+      outDir: "../dist",
+      emptyOutDir: true,
+    },
+    plugins: [],
+  };
+});
